adcd73bdcfdb2eafff69942fc32ad027ccb4cf88
matanmar <mmarcciano11@gmail.com>
2022-07-21 11:01:11 +0300

    Initial commit

---
+++ src/js/config.js
@@ -0,0 +1,5 @@
+export const API_URL = 'https://forkify-api.herokuapp.com/api/v2/recipes/';
+export const TIMEOUT_SEC = 10;
+export const RES_PER_PAGE = 10;
+export const KEY = '63752833-1352-439d-9f66-2c6e03ea0316';
+export const MODAL_CLOSE_SEC = 2.5;
